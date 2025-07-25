858cf9d2a1f7123d22eefae08d15617ca67f51db
Arman Procodrr <armanihsan224@gmail.com>
2025-07-25 18:30:01 +0500

    add local storage

--- Slices/todo.jsx
+++ Slices/todo.jsx
@@ -1,8 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Get todos from localStorage if they exist
+const getInitialTodos = () => {
+  const stored = localStorage.getItem('todos');
+  return stored ? JSON.parse(stored) : [];
+};
+
 const todoSlice = createSlice({
   name: 'todos',
-  initialState: [],
+  initialState: getInitialTodos(), // Load from localStorage
   reducers: {
     addTodo: (state, action) => {
       state.push({
@@ -10,17 +16,22 @@ const todoSlice = createSlice({
         text: action.payload,
         completed: false,
       });
+      localStorage.setItem('todos', JSON.stringify(state));
     },
     deleteTodo: (state, action) => {
-      return state.filter((todo) => todo.id !== action.payload);
+      const newState = state.filter((todo) => todo.id !== action.payload);
+      localStorage.setItem('todos', JSON.stringify(newState));
+      return newState;
     },
     toggleTodo: (state, action) => {
       const todo = state.find((todo) => todo.id === action.payload);
       if (todo) todo.completed = !todo.completed;
+      localStorage.setItem('todos', JSON.stringify(state));
     },
     editTodo: (state, action) => {
       const todo = state.find((todo) => todo.id === action.payload.id);
       if (todo) todo.text = action.payload.newText;
+      localStorage.setItem('todos', JSON.stringify(state));
     },
   },
 });
